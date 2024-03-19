bc77b2f001fa35b36f08c683c62d4876957f070b
simoneevacca <v.simone.vacca@gmail.com>
2024-03-19 14:40:04 +0100

    add an mail on page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 const { createApp } = Vue
 createApp({
     data() {
-        return {
 
+        
+        return {
+            randomMail: null,
+            callApi() {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        console.log(response.data.response);
+                        this.randomMail = response.data.response
+                    })
+            }
         }
+    },
+
+    mounted (){
+        this.callApi()
     }
 }).mount('#app')
