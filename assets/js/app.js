751eea2b72e08ba1fbfb42655f05b61704cd5c12
simoneevacca <v.simone.vacca@gmail.com>
2024-03-19 15:26:56 +0100

    add for loop for genarate different items

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,19 +6,25 @@ createApp({
         return {
             randomMail: null,
             number: 10,
+            mailArray: []
 
         }
     },
 
     methods: {
         callApi() {
-
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then((response) => {
-                    console.log(response.data.response);
-                    this.randomMail = response.data.response
-                })
+            for (let i = 0; i < 10; i++) {
+                
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        console.log(response.data.response);
+                        this.randomMail = response.data.response
+                        this.mailArray.push(response.data.response)
+                    })
+                    
+                }
+                console.log(this.mailArray);
 
         }
     },
